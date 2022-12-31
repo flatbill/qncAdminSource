64608836d80da353f3ba4fb43ee3ccc55b62a020
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2022-12-31 16:03:27 -0500

    Add files via upload

--- src/app/qncwwrd/qncwwrd.component.ts
+++ src/app/qncwwrd/qncwwrd.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { EventEmitter, Output     } from '@angular/core'
-import api from 'src/utils/api'
+import apiFauna from 'src/utils/apiFauna'
 
 @Component({
   selector: 'app-qncwwrd',
@@ -12,57 +12,89 @@ export class QncwwrdComponent implements OnInit {
   @Input() qidIn
   @Input() ruleNbrIn
   @Input() rulesIn  
+  @Input() groupsIn  
+  @Input() scoreboardsIn
+  @Input() questionsIn
+
   @Output() wwrJumpOut = new EventEmitter()
   msg1 = 'edit rule details.'
   rx = -1
+  gx = -1
   ruleObj = {}
   qtDbDataObj = {} 
   pendingAddRx = -1
   symDropDown    = '\u{25BC}'  
   verifyDelete = false
   fieldsDisabled = false
-
+  operArray = [
+    'less than'
+    ,'equal to'
+    ,'greater than'
+  ]
+  showScoreboardList = false
+  showGroupList = false
+  groupRound = '?'
+  groupQuestCount = '?'
+  wordAnswerOrAnswers = 'answers'
+  wordQuestionOrQuestions = 'questions'
+  wordsAboutGroup = 'group words'
   ngOnInit() {
-    // // billy temp:
-    // for (let i = 0; i < this.rulesIn.length; i++) {
-    //   if (!this.rulesIn[i].hasOwnProperty('ruleNbr')) {
-    //     this.rulesIn[i]['ruleNbr'] = this.fixRuleNbr()
-    //   } else {
-    //     if (this.rulesIn[i]['ruleNbr'] == '3377' ){
-    //       // billy this.rulesIn[i]['ruleNbr'] = this.fixRuleNbr()
-    //     } // end if
-    //   } // end if
-    // } // end for
-    // // end billy temp
-    
     console.log('running wwrd ngOnInit')
-    console.log(this.custIn,this.qidIn)
-    console.log(this.ruleNbrIn)
-    console.table(this.rulesIn)
-    console.log('42 ready to check ruleNbr')
-    if (this.ruleNbrIn==-1){
-      // he clicked add new on list screen.
+    if (this.ruleNbrIn==-1){ // he clicked add new on list screen.
       this.addButClick()
       this.rx = 0
-      this.msg1 ='New starter rule created. Ready for your changes.'
-    // } else {
     }  
     this.findRuleIx()
+    this.setRuleInfo()
+    this.setGroupInfo()
+    this.fieldValidations()
   } // end ngOnInit
 
-  // fixRuleNbr() { //temp billy
-  //   let ruleNbrMax = '001'
-  //   ruleNbrMax = 
-  //     Math.max.apply(Math, 
-  //                    this.rulesIn.map(function(r) { return r.ruleNbr }))
-  //   return (ruleNbrMax + 1).toString().padStart(3, '0')
-     
-  // } //end fixRuleNbr 
 
   findRuleIx(){
+    console.log('running findRuleIx')
     this.rx = this.rulesIn
     .findIndex(r => r.ruleNbr == this.ruleNbrIn)
   }
+
+  setRuleInfo(){
+    console.log('running setRuleInfo')
+    // this is part of the info about this rule.  see html.
+    if (this.rulesIn[this.rx].questCount==1) {
+      this.wordAnswerOrAnswers = 'answer'
+      this.wordQuestionOrQuestions = 'question adds'
+    } else {
+      this.wordAnswerOrAnswers = 'answers'
+      this.wordQuestionOrQuestions = 'questions add'
+    }  // end if
+  } // end setRuleInfo
+
+  setGroupInfo(){
+    console.log('running setGroupInfo')
+        // count questions per group:
+        let qCnt = 0
+        for (let i = 0; i<this.groupsIn.length; i++){
+           qCnt = this.questionsIn
+            .filter(q => q.subset.toLowerCase().trim() == this.groupsIn[i].groupName.toLowerCase().trim())
+            .length
+          this.groupsIn[i].qCount = qCnt
+        } // end for
+        console.log('90')
+        console.table(this.groupsIn)
+    
+    this.gx = this.groupsIn
+    .findIndex(g => g.groupName == this.rulesIn[this.rx].subset)
+    if (this.gx > -1) {
+      this.groupRound = this.groupsIn[this.gx].seq
+      this.groupQuestCount = this.groupsIn[this.gx].qCount 
+      this.wordsAboutGroup = ' questions and is in round '
+
+    } else {
+      this.groupRound = ''
+      this.groupQuestCount = '0'
+      this.wordsAboutGroup = ' questions. This group does not exist'
+    }
+  } // end setGroupInfo
 
   prevButClick(){
     console.log('running prevButClick')
@@ -77,6 +109,9 @@ export class QncwwrdComponent implements OnInit {
         loopRx = 0 // lets exit the while loop
       }
     } // end while
+    this.setRuleInfo()
+    this.setGroupInfo()
+    this.fieldValidations()
   } // end prevButClick
 
   nextButClick(){
@@ -91,17 +126,37 @@ export class QncwwrdComponent implements OnInit {
         loopRx = 9999 // lets exit the while loop
       }
     } // end while
+    this.setRuleInfo()
+    this.setGroupInfo()
+    this.fieldValidations()
   } // end nextButClick
 
-  saveButClick(){
-    console.log('running saveButClick')
-    this.msg1 = ''
-    this.saveRule()
-  } // end saveButClick
+  // saveButClick(){
+  //   console.log('running saveButClick')
+  //   this.msg1 = ''
+  //   this.saveRule('misc')
+  // } // end saveButClick
 
-  saveRule(){
-    console.log('running wwrd saveRule')
-    this.msg1 = this.msg1 + ' rule saved.'
+  fieldValidations(){
+    let x = this.validateScoreboard(this.rulesIn[this.rx].accum)
+    let y = this.validateGroup(this.rulesIn[this.rx].subset)
+  }
+
+  saveRule(parmIn){
+    console.log('7777777 running wwrd saveRule')
+
+    if (parmIn=='add'){
+      this.msg1 = 'New starter rule created. Ready for your changes.'
+    } else {
+      if (this.msg1.includes('autosaved')
+      && this.msg1.includes(parmIn) ){
+           this.msg1 = this.msg1 + '.' // autosaved same field again.
+      }  else {
+           this.msg1 = parmIn + ' changed. rule autosaved.'
+      } // end inner if
+    } // end outer if
+
+
     this.buildRuleObj() // uses current rx  billy fix
     if (this.pendingAddRx >= 0) {
       this.launchQtAddRule()
@@ -110,7 +165,8 @@ export class QncwwrdComponent implements OnInit {
       //  this is a changed rule:
       this.launchQtUpdateRule()
     }
-  }
+
+  }  // end saveRule
 
   buildRuleObj(){
     this.ruleObj = 
@@ -129,23 +185,22 @@ export class QncwwrdComponent implements OnInit {
   addButClick(){
     console.log('running wwrd addButClick')
     this.msg1 = 'edit this new rule.'
-    let newRuleNbr = '001'
+    let newRuleNbr = '1'
     if (this.rulesIn.length > 0) {
       //  set new rule nbr to one bigger than max rule nbr
       let ruleNbrMax = 
         Math.max.apply(Math, this.rulesIn.map(function(r) { return r.ruleNbr }))
-        newRuleNbr = (ruleNbrMax + 1).toString().padStart(3, '0')
-      //alert('new rule nbr: '+ newRuleNbr)
+        newRuleNbr = (ruleNbrMax + 1).toString()  
     } //end if
-    let newRuleScoreboardName = 'sb001'
-    let newRuleGroupName = 'grp001'
+    let newRuleScoreboardName = 'scoreboard1'
+    let newRuleGroupName = 'group1'
     this.rulesIn.push(
       {
         cust: this.custIn,
         qid: this.qidIn,
         ruleNbr: newRuleNbr,
         accum:   newRuleScoreboardName,
-        oper: '>',
+        oper: 'greater than',
         thresh: 0,
         subset: newRuleGroupName
       }
@@ -154,20 +209,18 @@ export class QncwwrdComponent implements OnInit {
       console.table(this.rulesIn)
       this.rx = this.rulesIn.length - 1
       this.pendingAddRx = this.rx // when he hits save, we use this.
-      this.msg1 = 'adding rule nbr: ' + this.rulesIn[this.rx].ruleNbr
-      this.saveRule() //auto save
-      this.msg1 ='New starter rule created. Ready for your changes.'
+      this.saveRule('add') //auto save
       this.rulesIn[this.rx].rFilterInOut = 'in'
       this.ruleNbrIn = newRuleNbr
+      this.fieldValidations()  // reset any prior rec fields marked as invalid.
       console.log('159 end of addButClick')
   } // end addButClick
 
   delButClick(){
     this.msg1=''
-    this.verifyDelete=true
+    this.verifyDelete = true
     this.fieldsDisabled = true
   } // end delButClick
-
 
   proceedWithDelete(){
     // delete rule from rule array.
@@ -205,36 +258,189 @@ export class QncwwrdComponent implements OnInit {
 
   ruleScoreboardChg(newScoreboard,rx)  {
     console.log('running ruleScoreboardChg to ',newScoreboard)
-    this.msg1 = 'scoreboard changed. '
+    let validScoreboard = this.validateScoreboard(newScoreboard)
+    if (!validScoreboard) {
+      return 
+    }
+
     this.rulesIn[rx].accum = newScoreboard.trim()
-    this.saveRule()
+    this.saveRule('scoreboard')
   } // end ruleScoreboardChg
 
   ruleOperChg(newOper,rx) {
     console.log('running ruleOperChg to ', newOper)
-    this.msg1 = 'oper changed. '
+    let validOper = this.validateOper(newOper)
+    if (!validOper) {
+      this.msg1 = 'oper invalid. '
+      return 
+    }
+    
     this.rulesIn[rx].oper = newOper.trim()
-    this.saveRule()
+    this.saveRule('oper')
+
   }
+
+    validateScoreboard(newScoreboard){
+    console.log('running validateScoreboard')
+    let el = document.getElementById('ruleScoreboard')
+    el.classList.remove('has-background-warning')
+
+    if (newScoreboard.trim().length<1 ){ 
+      el.classList.add('has-background-warning')
+      this.msg1="Invalid Scoreboard.  Can't be blank."
+      return false
+    } // end if
+
+    let position = 
+    this.scoreboardsIn.map(function(s) { return s.scoreboardName })
+    .indexOf(newScoreboard)
+    if (position <0 ){  
+      el.classList.add('has-background-warning')
+      this.msg1="Invalid Scoreboard.  Not in scoreboard list."
+      return false
+    } // end if
+
+    return true // fall here when all scoreboard validations are OK
+  } // end validateScoreboard
+
+  validateOper(newOper){
+    console.log('running validateOper')
+    let position = 
+    this.operArray.map(function(a) { return a })
+    .indexOf(newOper)
+  if (position > -1){  
+      return true
+    } else {
+      return false
+    }
+        // if (newOper == 'less than'
+    // ||  newOper == 'less than or equal'
+    // ||  newOper == 'equals'
+    // ||  newOper == 'greater than'
+    // ||  newOper == 'greater than or equal') {
+
+  } // end validateOper
+
+  validateThresh(newThresh){
+    console.log('running validateThresh')
+    let el = document.getElementById('ruleThresh')
+    el.classList.remove('has-background-warning')
+
+    if (Number(newThresh) >=0 && newThresh.trim().length>0 ){ 
+      return true
+    } else {
+      el.classList.add('has-background-warning')
+      this.msg1 = 'threshold invalid. Must be 0-99999'
+      return false
+    } // end if
+  } // end validateThresh
+
+  validateGroup(newGroup){
+    console.log('running validateGroup')
+    let el = document.getElementById('ruleGroup')
+    el.classList.remove('has-background-warning')
+
+    if (newGroup.trim().length<1 ){ 
+      el.classList.add('has-background-warning')
+      this.msg1="Invalid Group.  Can't be blank."
+      return false
+    } // end if
+
+    let position = 
+    this.groupsIn.map(function(s) { return s.groupName })
+    .indexOf(newGroup)
+    if (position <0 ){  
+      el.classList.add('has-background-warning')
+      this.msg1="Invalid Group.  Not in group list."
+      return false
+    } // end if
+
+    return true // fall here when all group validations are OK
+  } // end validateScoreboard
+
+  screenFieldGotFocus(focusFieldParmIn){
+    console.log('running screenFieldGotFocus',focusFieldParmIn)
+    if (focusFieldParmIn == 'ruleScoreboard' || focusFieldParmIn == 'scoreboardListItem' ){
+      this.showScoreboardList = true
+    } else {
+      this.showScoreboardList = false
+    }  //end if
+
+    if (focusFieldParmIn == 'ruleGroup' || focusFieldParmIn == 'groupListItem' ){
+      this.showGroupList = true
+    } else {
+      this.showGroupList = false
+    }  //end if
+
+   } // end screenFieldGotFocus
+
+   screenFieldLostFocus(whichField,ev){
+     console.log('running screenFieldLostFocus',whichField)
+
+     if (ev.relatedTarget == null){ // he is moving cursor off fields
+      this.showScoreboardList = false
+      this.showGroupList = false
+     } // end if
+
+    if (ev.relatedTarget != null
+    && !ev.relatedTarget.id.includes('scoreboardListItem')) {
+      // he is moving to a different input field
+      this.showScoreboardList = false
+    } // end if
+
+    if (ev.relatedTarget != null
+    && !ev.relatedTarget.id.includes('groupListItem')) {
+        // he is moving to a different input field
+        this.showGroupList = false
+    }
+  
+   } // end screenFieldLostFocus
+
+
+  operButClick(ruleOperParmIn,rx){
+    console.log('running operButClick')
+    console.log(ruleOperParmIn)
+    this.ruleOperChg(ruleOperParmIn,rx)
+  } //operButClick
+
+  scoreboardChosen(newScoreboard,rx){
+    console.log('running scoreboardChosen')
+    this.showScoreboardList = false
+    this.ruleScoreboardChg(newScoreboard,rx)
+  } // end scoreboardChosen
+
+  groupChosen(newGroup,rx){
+    console.log('running groupChosen')
+    this.showGroupList = false
+    this.ruleGroupChg(newGroup,rx)
+    this.setGroupInfo()
+  } // end groupChosen
+
 
   ruleThreshChg(newThresh,rx) {
     console.log('running ruleThreshChg to ', newThresh)
-    this.msg1 = 'threshold changed. '
+    let validThresh = this.validateThresh(newThresh)
+    if (!validThresh) {
+      return 
+    }
+
     this.rulesIn[rx].thresh = newThresh.trim()
-    this.saveRule()
+    this.saveRule('threshold')
   }
 
   ruleGroupChg(newGroup,rx) {
     console.log('running ruleGroupChg to ',newGroup)
-    this.msg1 = 'group changed. '
+    let validGroup = this.validateGroup(newGroup)
+    if (!validGroup) {
+      return 
+    }
     this.rulesIn[rx].subset = newGroup.trim()
-    this.saveRule()
+    this.saveRule('group')
   }
 
   launchQtAddRule(){
     console.log('running  wwrd launchQtAddRule')
-    //this.ruleObj['ruleNbr'] = '001'
-    api.qtWriteRule(this.ruleObj)
+    apiFauna.qtWriteRule(this.ruleObj)
     .then ((qtDbRtnObj) => { this.qtDbDataObj = qtDbRtnObj.data})
     // return from this on-the-fly function is implied  
     .catch(() => {
@@ -245,7 +451,7 @@ export class QncwwrdComponent implements OnInit {
 
   launchQtDeleteRule(){
     console.log('running  wwrd launchQtDeleteRule')
-    api.qtDeleteRule(this.ruleObj)
+    apiFauna.qtDeleteRule(this.ruleObj)
     .then ((qtDbRtnObj) => {this.qtDbDataObj = qtDbRtnObj.data})
     // return from this on-the-fly function is implied  
     .catch(() => {
@@ -256,7 +462,7 @@ export class QncwwrdComponent implements OnInit {
 
   launchQtUpdateRule(){
     console.log('running  wwrd launchQtUpdateRule')
-    api.qtUpdateRule(this.ruleObj)
+    apiFauna.qtUpdateRule(this.ruleObj)
     .then ((qtDbRtnObj) => {this.qtDbDataObj = qtDbRtnObj.data})
     // return from  on-the-fly function is implied. 
     .catch(() => {
